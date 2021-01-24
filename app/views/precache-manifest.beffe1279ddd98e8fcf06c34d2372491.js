0f360527e064651bf70d40562eac168c8d3604e5
hotspoon <fariez18@outlook.com>
2021-01-24 22:01:20 +0700

    Initial commit

---
+++ app/views/precache-manifest.beffe1279ddd98e8fcf06c34d2372491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472e3721cf1c53609448fe440e6022e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4cbfe940e84cbf83bbd",
+    "url": "/static/css/main.da17f1bf.chunk.css"
+  },
+  {
+    "revision": "4e7aa38e43e95e05868f",
+    "url": "/static/js/2.f0d98e9c.chunk.js"
+  },
+  {
+    "revision": "92bfebbc9186106791d506e48cb876d1",
+    "url": "/static/js/2.f0d98e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4cbfe940e84cbf83bbd",
+    "url": "/static/js/main.30071d33.chunk.js"
+  },
+  {
+    "revision": "5820d58c60ab3b1375ad",
+    "url": "/static/js/runtime-main.99b514f4.js"
+  }
+]);
